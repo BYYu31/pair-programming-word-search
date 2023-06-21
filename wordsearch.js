98c28cf3cd4a1e93b59765b3f51346e63ffe97ab
Bo Yang Yu <boyangyu93@gmail.com>
2023-06-20 22:19:38 -0400

    wordsearch function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
+// collab with Rebeccab
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    if (letters.length === 0 || !word) {
+        return false;
     }
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true 
+        }
+    }
+
+    //transpose
+
+    let result = [];
+    for (let j = 0; j < letters.length; j++) {
+        for (let i = 0;  i < letters[i].length; i++) {
+            if (!Array.isArray(result[i])) {
+                result[i] = [];
+            }
+            result[i][j] = letters[j][i];
+        }
+    }
+
+    let vertical = result.map(ls => ls.join(''));
+    for (const index of vertical) {
+        if (index.includes(word)) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 module.exports = wordSearch
